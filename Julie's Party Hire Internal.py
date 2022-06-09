04d99648a96e5edb1ff9475b2044e5490b0c9114
Ved <st21120@ormiston.school.nz>
2022-06-10 10:12:20 +1200

    commit 2

--- Julie's Party Hire Internal.py
+++ Julie's Party Hire Internal.py
@@ -2,6 +2,22 @@
 #Author: Ved
 #Date: 10 June 2022
 
-
+#Import tkinter
 from tkinter import*
 from tkinter import ttk
+
+root=Tk()
+
+#Create quit subroutine
+def quit():
+    root.destroy()
+
+
+#Create labels
+lbltitle = ttk.Label(root, text="Julies Party Hire", font=("Helvetica 30 bold"))
+
+lbltitle.grid(row=0, column=0, columnspan=2)
+
+
+root.geometry("500x450")
+root.mainloop()
